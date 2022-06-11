74ccdf29c84866a6ed68212424f92f9665f6b531
Victor Trintim <49760038+Trintim@users.noreply.github.com>
2022-06-10 22:28:11 -0300

    Update and finished

--- admin-template/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/template/Layout";
-
-export default function Home() {
-  return (
-    <Layout titulo="Pagina Inicial" subtitulo="Estamos construindo o template admin!!">
-      <h3>Conteudo!!</h3>
-    </Layout>
-  )
-}
